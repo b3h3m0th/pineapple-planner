2367fdef924da675c58a3ca50924c75449e3b2ad
simonostini <simonostini@gmail.com>
2025-01-30 23:31:53 +0100

    added demo cqrs query

---
+++ PineapplePlanner/PineapplePlanner.Application/Todos/GetTodos.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using PineapplePlanner.Domain.Entities;
+using System.Diagnostics;
+
+namespace PineapplePlanner.Application
+{
+    /// <summary>
+    /// Returns all employees from the database
+    /// </summary>
+    public class GetTodos
+    {
+        public record Query() : IRequest<Result>;
+
+        public class Result
+        {
+            public List<Todo> Todos { get; set; } = new List<Todo>();
+        }
+
+        public class QueryHandler : IRequestHandler<Query, Result>
+        {
+            public async Task<Result> Handle(Query request, CancellationToken cancellationToken)
+            {
+                Result result = new Result();
+
+                try
+                {
+                    await Task.Delay(500);
+
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    Trace.WriteLine(ex);
+                }
+
+                return result;
+            }
+        }
+    }
+}
