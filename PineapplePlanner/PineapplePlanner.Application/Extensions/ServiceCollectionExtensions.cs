b54d655d73f99de703c9230a64bc6f23464355a9
simonostini <simonostini@gmail.com>
2025-02-06 13:18:43 +0100

    vault backup: 2025-02-06 13:18:43

--- PineapplePlanner/PineapplePlanner.Application/Extensions/ServiceCollectionExtensions.cs
+++ PineapplePlanner/PineapplePlanner.Application/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using PineapplePlanner.Application.Interfaces;
 using PineapplePlanner.Application.Repositories;
 
 namespace PineapplePlanner.UI.Extensions
@@ -7,6 +8,7 @@ namespace PineapplePlanner.UI.Extensions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
+            services.AddInfrastructureServices();
             //services.AddScoped<IBaseRespository<IBaseFirestoreData>, BaseRepository<IBaseFirestoreData>>();
             services.AddScoped<ITaskRepository, TaskRepository>();
 
