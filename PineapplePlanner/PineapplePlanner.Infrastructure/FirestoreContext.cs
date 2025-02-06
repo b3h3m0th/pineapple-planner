32e1ec7775edf548a6306829c10b95eec457badc
simonostini <simonostini@gmail.com>
2025-02-06 10:00:41 +0100

    vault backup: 2025-02-06 10:00:41

--- PineapplePlanner/PineapplePlanner.Infrastructure/FirestoreContext.cs
+++ PineapplePlanner/PineapplePlanner.Infrastructure/FirestoreContext.cs
@@ -4,33 +4,13 @@ namespace PineapplePlanner.Infrastructure
 {
     public sealed class FirestoreContext
     {
-        private static FirestoreContext? _instance = null;
-        private static readonly object _lock = new object();
         private readonly FirestoreDb _firestoreDb;
 
         // Private constructor to prevent direct instantiation
-        private FirestoreContext()
+        public FirestoreContext()
         {
             string projectId = "your-firebase-project-id";
             _firestoreDb = FirestoreDb.Create(projectId);
-        }
-
-        public static FirestoreContext Instance
-        {
-            get
-            {
-                if (_instance == null)
-                {
-                    lock (_lock)
-                    {
-                        if (_instance == null)
-                        {
-                            _instance = new FirestoreContext();
-                        }
-                    }
-                }
-                return _instance;
-            }
         }
 
         public FirestoreDb GetDb() => _firestoreDb;
