3a230570d7ab2bf921cf113d0158179df2cde792
simonostini <simonostini@gmail.com>
2025-02-06 13:08:38 +0100

    vault backup: 2025-02-06 13:08:38

--- PineapplePlanner/PineapplePlanner.UI/Pages/Home.razor.cs
+++ PineapplePlanner/PineapplePlanner.UI/Pages/Home.razor.cs
@@ -5,13 +5,15 @@ namespace PineapplePlanner.UI.Pages
     public partial class Home
     {
         private GetTodos.Result _todosResult = new GetTodos.Result();
+        private List<Domain.Entities.Task> _tasks = new List<Domain.Entities.Task>();
         private string _error = string.Empty;
 
-        protected override async Task OnParametersSetAsync()
+        protected override async System.Threading.Tasks.Task OnParametersSetAsync()
         {
             try
             {
                 _todosResult = await _mediator.Send(new GetTodos.Query());
+                _tasks = await _taskRepository.GetAllAsync();
             }
             catch (Exception e)
             {
