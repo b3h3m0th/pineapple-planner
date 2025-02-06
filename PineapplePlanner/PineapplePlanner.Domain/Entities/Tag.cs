4b95387df989d640d04d87c62088027e98399892
simonostini <simonostini@gmail.com>
2025-02-06 12:00:38 +0100

    vault backup: 2025-02-06 12:00:38

---
+++ PineapplePlanner/PineapplePlanner.Domain/Entities/Tag.cs
@@ -0,0 +1,9 @@
+﻿namespace PineapplePlanner.Domain.Entities
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public string Color { get; set; } = string.Empty;
+    }
+}
