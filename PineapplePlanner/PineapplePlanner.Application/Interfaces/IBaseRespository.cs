9bb684907d2968f7fab6fb23508dff8446ee52ce
simonostini <simonostini@gmail.com>
2025-02-06 12:43:38 +0100

    vault backup: 2025-02-06 12:43:38

--- PineapplePlanner/PineapplePlanner.Application/Interfaces/IBaseRespository.cs
+++ PineapplePlanner/PineapplePlanner.Application/Interfaces/IBaseRespository.cs
@@ -5,15 +5,10 @@ namespace PineapplePlanner.Application.Interfaces
     public interface IBaseRespository<T> where T : IBaseFirestoreData
     {
         Task<List<T>> GetAllAsync();
-
         Task<object> GetAsync(T entity);
-
         Task<T> AddAsync(T entity);
-
         Task<T> UpdateAsync(T entity);
-
         Task DeleteAsync(T entity);
-
         Task<List<T>> QueryRecordsAsync(object query);
     }
 }
