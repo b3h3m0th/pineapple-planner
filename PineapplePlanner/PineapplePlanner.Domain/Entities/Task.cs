4794a6746bc908eaba642c7fadb67bd7ea413840
simonostini <simonostini@gmail.com>
2025-02-06 11:59:38 +0100

    vault backup: 2025-02-06 11:59:38

---
+++ PineapplePlanner/PineapplePlanner.Domain/Entities/Task.cs
@@ -0,0 +1,12 @@
+﻿namespace PineapplePlanner.Domain.Entities
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? Reminder { get; set; }
+        public DateTime? DateDeleted { get; set; }
+    }
+}
