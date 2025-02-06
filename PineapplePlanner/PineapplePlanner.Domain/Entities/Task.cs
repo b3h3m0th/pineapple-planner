ad773777513170545490ca9ba02f1bfe17f97600
simonostini <simonostini@gmail.com>
2025-02-06 12:03:38 +0100

    vault backup: 2025-02-06 12:03:38

--- PineapplePlanner/PineapplePlanner.Domain/Entities/Task.cs
+++ PineapplePlanner/PineapplePlanner.Domain/Entities/Task.cs
@@ -5,9 +5,10 @@
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
-        public DateTime DateCreated { get; set; }
+        public DateTime CompletedAt { get; set; }
         public DateTime? Reminder { get; set; }
-        public DateTime? DateDeleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
         public List<Tag> Tags { get; set; } = new List<Tag>();
     }
 }
