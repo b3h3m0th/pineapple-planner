7237725dcd422989f0a4142d18383101ca995989
simonostini <simonostini@gmail.com>
2025-02-06 12:32:38 +0100

    vault backup: 2025-02-06 12:32:38

--- PineapplePlanner/PineapplePlanner.Application/Interfaces/IBaseRespository.cs
+++ PineapplePlanner/PineapplePlanner.Application/Interfaces/IBaseRespository.cs
@@ -1,42 +1,17 @@
-﻿using PineapplePlanner.Domain.Interfaces;
-
-namespace PineapplePlanner.Application.Interfaces
+﻿namespace PineapplePlanner.Application.Interfaces
 {
     public interface IBaseRespository<T>
     {
-        Task<List<T>> GetAllAsync<T>() where T : IBaseFirestoreData;
+        Task<List<T>> GetAllAsync();
 
-        /// <summary>
-        /// Gets a record from the repository.
-        /// </summary>
-        /// <param name="entity"></param>
-        /// <returns>a record of type T</returns>
-        Task<object> GetAsync<T>(T entity) where T : IBaseFirestoreData;
+        Task<object> GetAsync(T entity);
 
-        /// <summary>
-        /// Adds a record to the repository.
-        /// </summary>
-        /// <param name="entity"></param>
-        /// <returns>a record of type T</returns>
-        Task<T> AddAsync<T>(T entity) where T : IBaseFirestoreData;
+        Task<T> AddAsync(T entity);
 
-        /// <summary>
-        /// Updates a record in the repository.
-        /// </summary>
-        /// <param name="entity"></param>
-        /// <returns>a record of type T</returns>
-        Task<T> UpdateAsync<T>(T entity) where T : IBaseFirestoreData;
+        Task<T> UpdateAsync(T entity);
 
-        /// <summary>
-        /// Adds a record to the repository.
-        /// </summary>
-        /// <param name="entity"></param> S
-        Task DeleteAsync<T>(T entity) where T : IBaseFirestoreData;
+        Task DeleteAsync(T entity);
 
-        /// <summary>
-        /// Query all record from the repository.
-        /// </summary> 
-        /// <returns>a records of type T</returns>
-        Task<List<T>> QueryRecordsAsync<T>(Query query) where T : IBaseFirestoreData;
+        Task<List<T>> QueryRecordsAsync(object query);
     }
 }
