9485f8dbf9d841049056c1bc156d63f0efaff528
simonostini <simonostini@gmail.com>
2025-02-13 17:18:57 +0100

    Create task/task detail

---
+++ PineapplePlanner/PineapplePlanner.UI/Components/TaskDetail.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace PineapplePlanner.UI.Components
+{
+    public partial class TaskDetail
+    {
+        [Parameter]
+        public bool IsOpen { get; set; }
+
+        [Parameter]
+        public EventCallback<bool> IsOpenChanged { get; set; }
+
+        [Parameter]
+        public Domain.Entities.Task? Task { get; set; }
+
+    }
+}
