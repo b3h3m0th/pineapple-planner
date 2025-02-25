88b62b96d520f32373623b7d06926ece1c50842d
simonostini <simonostini@gmail.com>
2025-02-25 17:19:37 +0100

    fixed project reference

--- PineapplePlanner/PineapplePlanner.Domain.UnitTests/Shared/ResultBaseTests.cs
+++ PineapplePlanner/PineapplePlanner.Domain.UnitTests/Shared/ResultBaseTests.cs
@@ -1,7 +1,18 @@
-﻿namespace PineapplePlanner.Domain.UnitTests.Shared
+﻿using PineapplePlanner.Domain.Shared;
+
+namespace PineapplePlanner.Domain.UnitTests.Shared
 {
     public class ResultBaseTests
     {
+        [Fact]
+        public void ResultBase_Success_ShouldSetIsSuccessTrue()
+        {
+            // Arrange & Act
+            ResultBase result = ResultBase.Success();
 
+            // Assert
+            Assert.True(result.IsSuccess);
+            Assert.Empty(result.Errors);
+        }
     }
 }
