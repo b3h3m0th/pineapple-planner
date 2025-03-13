6e9abd26460bc3f5d3032ddd3a88f4032bca6754
DKxd <35139906+Stroicikas@users.noreply.github.com>
2025-03-13 02:09:18 +0100

    fix calendar not changing languages+added german/spanish/russian languages

--- PineapplePlanner/PineapplePlanner.UI/Localization/Culture.cs
+++ PineapplePlanner/PineapplePlanner.UI/Localization/Culture.cs
@@ -5,5 +5,7 @@
         public const string English = "en-US";
         public const string Swedish = "sv-SE";
         public const string German = "de-DE";
+        public const string Spanish = "es-ES";
+        public const string Russian = "ru-RU";
     }
 }
