d73bf92a3d32d708114ad36dde2b1ca10428e372
simonostini <simonostini@gmail.com>
2025-03-19 13:39:11 +0100

    resetted all files accidenally modified

--- PineapplePlanner/PineapplePlanner.UI/Localization/Culture.cs
+++ PineapplePlanner/PineapplePlanner.UI/Localization/Culture.cs
@@ -1,18 +1,17 @@
 ﻿namespace PineapplePlanner.UI.Localization
 {
-  public static class Culture
-  {
-    public const string English = "en-US";
-    public const string Swedish = "sv-SE";
-    public const string German = "de-DE";
-    public const string Spanish = "es-ES";
-    public const string Russian = "ru-RU";
-    public const string Japanese = "ja-JP";
-    public const string Estonian = "et-EE";
-    public const string Ukrainian = "ua-UA";
-    public const string Polish = "pl-PL";
-    public const string French = "fr-FR";
-    public const string Lithuanian = "lt-LT";
-    public const string Urdu = "ur-PK";
-  }
+    public static class Culture
+    {
+        public const string English = "en-US";
+        public const string Swedish = "sv-SE";
+        public const string German = "de-DE";
+        public const string Spanish = "es-ES";
+        public const string Japanese = "ja-JP";
+        public const string Estonian = "et-EE";
+        public const string Ukrainian = "ua-UA";
+        public const string Polish = "pl-PL";
+        public const string French = "fr-FR";
+        public const string Lithuanian = "lt-LT";
+        public const string Urdu = "ur-PK";
+    }
 }
